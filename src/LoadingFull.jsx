909b873ea682d9f36dfa70de72b4eae7f4d92171
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2025-06-13 23:45:32 +0530

    Add files via upload

---
+++ src/LoadingFull.jsx
@@ -0,0 +1,8 @@
+
+import { Spin } from 'antd';
+const LoadingFull = () => (
+    <div>
+        <Spin fullscreen={true} size="large" />
+    </div>
+);
+export default LoadingFull;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+
+import SmallCalender from "./SmallCalender"
+
+const Sidebar = () => {
+return (
+    <aside className="border  px-5 w-50">
+        {/* Create Event Dummy Button for testing  */}
+        {/* <CreateEventButton /> */}
+        <SmallCalender />
+        {/*  */}
+    </aside>
+)
+}
+
+export default Sidebar
